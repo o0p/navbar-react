38f28a3e5b83d6b47ba1bcddff8593ff49dfe601
Ruslan <rus@gmx.com>
2021-05-27 01:08:06 +0100

    Refactor links

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,23 +15,19 @@ const Navbar = () => {
         </div>
         <div className="links-container show-container">
           <ul className="links">
-            <li>
-              <a href="#">home</a>
-            </li>
-            <li>
-              <a href="#">about</a>
-            </li>
-            <li>
-              <a href="#">contact</a>
-            </li>
-            <li>
-              <a href="#">products</a>
-            </li>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <ul className="social-icons">
           <li>
-            <a href="https://twitter.com/">
+            <a href="https://twitter.com">
               <FaTwitter />
             </a>
           </li>
